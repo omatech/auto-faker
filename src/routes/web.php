b9f2d7c3b706ea56c81779e31fb6223f19d0a890
Agusti Pons <apons@omatech.com>
2019-04-30 16:20:10 +0200

    autofaker config

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Http\Request;
 
 Route::get('/markup/{path}.html', function (Request $request, $path) {
-    $autofaker=new Omatech\AutoFaker\AutoFaker(file_get_contents(config_path("/fake-data/$path.yaml")));
-    $autofaker->setFakerFormat(file_get_contents(config_path("/fake-data/fake_record_format.yaml")));
+    $autofaker=new Omatech\AutoFaker\AutoFaker(file_get_contents(config_path("autofaker/$path.yaml")));
+    $autofaker->setFakerFormat(file_get_contents(config_path("autofaker/fake_record_format.yaml")));
     $data=$autofaker->getData();
     if ($request->input('debug-data')=='true') dd($data);
     return view("markup.$path", $data);
